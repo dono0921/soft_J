b6bfe8149a10ccef230ea77aab84689880fcbfc4
dono <34529390+dono0921@users.noreply.github.com>
2018-01-18 11:32:11 +0900

    Add files via upload

--- misslog.c
+++ misslog.c
@@ -10,17 +10,30 @@ FILE *fi;
 		printf("間違ってます");
 		fi=fopen("misslog.txt","a");		//間違えた問題を保存
 		fprintf(fi,"%d",&i);
-		fclose();
+		fclose(fi);
 	}else{
 		printf("正解です");
 	}
+}
 
 void misslog(){
-	FILE *fi;
+	FILE *fi,*fo;
+	char misseibun,kotae,answer2;
 
-	fi=fopen("misslog.txt","r");
+	fi=fopen("misslog.txt","r");			//間違えた問題を表示
 		for(;;){
-		c=fgetc(fi);
-		if(c==EOF) exit(1);
-		printf()
+		fgets(misseibun,40,fi);
+		puts(misseibun);
+		scanf("%s",answer2);
+		
+		if(strcmp(kotae,answer2)==0){
+			prinf("正解です");
+			fo=fopen("misslog.txt","w");
+			for(i=0;i<200;i++) fprintf(fo,"%d",&i+1);
+		}else{
+			printf("間違ってます");
 		}
+		}
+		fclose(fi);
+		fclose(fo);
+}
