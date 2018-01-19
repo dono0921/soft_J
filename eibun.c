10fb7bb4914cfc8890e1ff78914d437b0d2eddd9
dono <34529390+dono0921@users.noreply.github.com>
2018-01-19 12:14:27 +0900

    Add files via upload

---
+++ eibun.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define DATAMAX 30
+
+void toeic();
+void eiken();
+void exit();
+
+typedef struct{
+	char eibun[200];
+	char kotae[200];
+	char nihonbun[200];
+	int  score;
+}record;
+
+record data[DATAMAX];
+
+int main(){
+
+	int i=0,score=0,k=0,l=0;
+	FILE *fi,*fj,*fk,*fl;
+	char answer[DATAMAX];
+
+	fi=fopen("toeic-eibun.txt","r");
+	fj=fopen("toeic-nihonbun.txt","r");
+	fk=fopen("toeic-kotae.txt","r");
+
+	for(i=0;i<DATAMAX;i++){
+		fgets(data[i].eibun,200,fi);
+		fgets(data[i].nihonbun,200,fj);
+		fgets(data[i].kotae,200,fk);
+	}
+	fclose(fi);
+	fclose(fj);
+	fclose(fk);
+
+	for(i=0;i<DATAMAX;i++){
+		printf("%s",data[i].eibun);
+		printf("%s",data[i].nihonbun);
+		fgets(answer,DATAMAX,stdin);
+
+	if(strcmp(answer,data[i].kotae)==0){
+		printf("正解\n");
+		score++;
+	}else{
+		printf("不正解\n");
+		if(k=i){
+		fl=fopen("misslog.txt","a");		//間違えた問題を保存
+		fprintf(fl,"%d\n",i);
+		fclose(fl);
+		}else{
+				}
+			printf("答え: %s",data[i].kotae);
+}
+	printf("正解数は %d / %d \n",score,DATAMAX);
+}
+}
+
+
+
+
+
+
+
+
